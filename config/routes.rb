a779251cc9a2d2a6a4787717fe600fcca6763033
Noah Prescott <you@example.com>
2019-06-08 22:48:07 -0400

    Add signup view and change to stylesheet pack tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "users#new"
+
+  get "users/new" => "users#new", as: :new_user
+  post "users" => "users#create"
 end
