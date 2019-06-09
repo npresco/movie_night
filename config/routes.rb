f606315ea63881c623dca11efa914ecacba4a8d0
Noah Prescott <noah.prescott@gmail.com>
2019-06-08 22:59:35 -0400

    User (#1)
    * Add user with password
    
    * Add users controller
    
    * Add signup view and change to stylesheet pack tag
    
    * Add sessions controller
    
    * Add sessions controller and login view
    
    * Add simple authorize and user header

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "users#new"
+
+  # Users
+  get "users/new"  => "users#new", as: :new_user
+  post "users"     => "users#create"
+
+  # Login
+  get "/login"     => "sessions#new"
+  post "/login"    => "sessions#create"
+  delete "/logout" => "sessions#destroy"
 end
