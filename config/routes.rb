9f6e2fba11f44e2d11b68dd66a489a38acf8c7d5
Noah Prescott <noah.prescott@gmail.com>
2019-06-15 19:15:53 -0400

    Add movie and watchlist models (#5)

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   get "/login"     => "sessions#new"
   post "/login"    => "sessions#create"
   delete "/logout" => "sessions#destroy"
+
+  resources :movies
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+File.open("./app/assets/movies.txt", "r") do |file|
+  file.each_line do |line|
+    Movie.create(title: line)
+  end
+end
