01d4d11949e588628c37c5013520c6331e4e9c5e
Noah Prescott <noah.prescott@gmail.com>
2019-06-15 17:51:17 -0400

    Add simple just watch wrapper class (#4)

---
+++ lib/just_watch_api.rb
@@ -0,0 +1,14 @@
+require 'httparty'
+
+class JustWatchApi
+  attr_reader :response
+
+  # Only searches on title for now  
+  def initialize(query)
+    @response = HTTParty.get("https://apis.justwatch.com/content/titles/en_US/popular?body={%22page_size%22:5,%22page%22:1,%22query%22:%22#{query}%22,%22content_types%22:[%22show%22,%22movie%22]}")
+  end
+
+  def items
+    @items ||= @response["items"]
+  end
+end
